a44d2cc8f813a62981795dc334c79ac3863ef0c2
Fynn Hauptmeier <fynn.hauptmeier@gmail.com>
2021-02-01 22:11:19 +0100

    Update z3 to version 4.8.9 (added str.to.int and other string functions)

--- cova/localLibs/install_local_libs.sh
+++ cova/localLibs/install_local_libs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 LPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-mvn install:install-file -Dfile=$LPATH/com.microsoft.z3.jar -DgroupId=z3Prover -DartifactId=z3 -Dversion=4.5.0 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=$LPATH/com.microsoft.z3.jar -DgroupId=z3Prover -DartifactId=z3 -Dversion=4.8.9 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=$LPATH/boomerang.jar -DgroupId=de.fraunhofer.iem -DartifactId=boomerangPDS -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=$LPATH/soot-infoflow.jar -DgroupId=de.tud.sse -DartifactId=soot-infoflow -Dversion=2.5.1 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=$LPATH/soot-infoflow-android.jar -DgroupId=de.tud.sse -DartifactId=soot-infoflow-android -Dversion=2.5.1 -Dpackaging=jar -DgeneratePom=true
